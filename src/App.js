63f4b5329676261a5a845a778a22ebddb8557c4a
CodeSandbox <support@codesandbox.io>
2024-01-20 04:38:39 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./styles.css";
+// import { useState } from "react";
+
+import Counter from "./components/Counter";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return <button onClick={increment}> Button {count} </button>;
+}
