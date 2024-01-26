39fc43862917f9e2583d21914c47c6b83d9f03d8
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-01-26 09:49:06 +0000

    If statement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
 
 export default function App() {
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+  useEffect(() => {
+    console.log(condition);
+  }, [condition]);
+  const renderCondition = condition ? "True" : "False";
   return (
     <div className="App">
-      <Counter name="click1" />
-      <Counter name="click2" />
+      <h1>Kossie</h1>
+      <div>{renderCondition}</div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
