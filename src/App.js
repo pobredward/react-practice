91e437da045d51b419977ebd5ff98e4d0928523e
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-01-26 10:21:26 +0000

    loop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useState, useEffect } from "react";
-
-// import Counter from "./components/Counter";
+import React from "react";
+import "./styles.css";
 
 export default function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = () => setCondition(!condition);
-  useEffect(() => {
-    console.log(condition);
-  }, [condition]);
-  const renderCondition = condition ? "True" : "False";
+  const movies = [
+    { title: "Kossie1", year: 2020 },
+    { title: "Kossie2", year: 2021 },
+    { title: "Kossie3", year: 2022 },
+  ];
+
+  const renderMovies = movies.map((movie) => (
+    <div className="movie" key={movie.title}>
+      <div className="movie-title">{movie.title}</div>
+      <div className="movie-year">{movie.year}</div>
+    </div>
+  ));
+
   return (
     <div className="App">
-      <h1>Kossie</h1>
-      <div>{renderCondition}</div>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie List</h1>
+      {renderMovies}
     </div>
   );
 }
