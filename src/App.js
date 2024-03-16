d4ca5ead121f61147ada9c6684d67024aae595e4
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-03-16 22:07:07 +0000

    20240317-1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Root</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
