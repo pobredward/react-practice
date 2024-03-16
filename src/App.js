a93cae23465d59cec0493d67207ce0b507f366c0
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-03-16 21:17:46 +0000

    Reset All

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React, { Component } from "react";
-import AddNumberRoot from "./components/AddNumberRoot";
-import DisplayNumberRoot from "./components/DisplayNumberRoot";
+import React from "react";
 import "./styles.css";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 1,
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Root</h1>
-        <AddNumberRoot></AddNumberRoot>
-        <DisplayNumberRoot></DisplayNumberRoot>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Root</h1>
+    </div>
+  );
 }
