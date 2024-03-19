7f1ee72fe17e3698a7d3455768776a83976b685c
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-03-19 02:28:57 +0000

    Reset All

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React from "react";
 import "./styles.css";
-import useFetch from "./useFetch";
-
-const baseUrl = "https://jsonplaceholder.typicode.com";
 
 export default function App() {
-  const { data: userData } = useFetch(baseUrl, "users");
-  const { data: postData } = useFetch(baseUrl, "posts");
-
-  return (
-    <div className="App">
-      <h1>User</h1>
-      {userData && <pre>{JSON.stringify(userData[0], null, 2)}</pre>}
-      <h1>Post</h1>
-      {userData && <pre>{JSON.stringify(postData[0], null, 2)}</pre>}
-    </div>
-  );
+  return <div className="App"></div>;
 }
